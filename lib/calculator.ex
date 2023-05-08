41553bcaa8bd0fea52ad4d7b1d2ed51bf459996c
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-05-09 03:45:31 +0530

    Break tests into their own module

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -77,14 +77,19 @@ defmodule Calculator.Client do
 
     client
   end
+end
+
+defmodule Calculator.Test do
+  alias Calculator.Client
+  alias Calculator.Pooler
 
   def test() do
-    %Client{server_pid: self()}
-    |> Client.add({2, 7})
-    |> Client.div({8, 0})
-    |> Client.sub({20, -100})
-    |> Client.div({8, 5})
-    |> Client.mul({4, 8})
+    test(Pooler.start())
+
+    # you should see most tasks outputs after 5 seconds
+    # and then depending on how congested a particular
+    # server is, some client queries to that server may
+    # time out before the server can respond to them
   end
 
   def test(server_pid) do
