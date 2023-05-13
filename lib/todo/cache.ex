89d7e265e0cc77ac9305c543d2101cf950a360ff
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-05-13 22:27:04 +0530

    Implement a todo.server cache

---
+++ lib/todo/cache.ex
@@ -0,0 +1,43 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  @impl GenServer
+  def init(_), do: {:ok, Map.new()}
+
+  @impl GenServer
+  def handle_call({:get_or_create, key}, _, cache) do
+    server_pid = Map.get(cache, key) || Todo.Server.start()
+    {:reply, server_pid, Map.put(cache, key, server_pid)}
+  end
+
+  def start() do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def server_process(key) do
+    GenServer.call(__MODULE__, {:get_or_create, key})
+  end
+end
+
+defmodule Todo.Cache.Test do
+  def test(n \\ 100) do
+    with {:ok, _} <- Todo.Cache.start() do
+      Enum.each(1..n, &Todo.Cache.server_process("List number #{&1}"))
+    end
+
+    alice = Todo.Cache.server_process("List number 1")
+    bob = Todo.Cache.server_process("List number 2")
+
+    Todo.Server.add_entry(alice, %{date: ~D[2023-05-03], title: "get groceries"})
+    Todo.Server.add_entry(alice, %{date: ~D[2023-05-02], title: "write journal"})
+    Todo.Server.add_entry(alice, %{date: ~D[2023-05-05], title: "buy coconut"})
+    Todo.Server.add_entry(alice, %{date: ~D[2023-05-03], title: "sell feet pics"})
+
+    Todo.Server.add_entry(bob, %{date: ~D[2023-05-03], title: "redeem coupon"})
+    Todo.Server.add_entry(bob, %{date: ~D[2023-05-02], title: "sing a song"})
+
+    Todo.Server.entries(alice, ~D[2023-05-03])
+    Todo.Server.entries(bob, ~D[2023-05-03])
+    Todo.Server.entries(bob, ~D[2023-05-10])
+  end
+end
