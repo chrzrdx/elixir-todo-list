7debf3d41e721644dbe4fc136f2bfbfd1ff7384c
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-05-17 23:55:36 +0530

    Make the db process name & persistence folders configurable (DI)

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,47 +1,45 @@
 defmodule Todo.Database do
+  alias __MODULE__
   use GenServer
 
-  @persist_db "./db/data"
+  defstruct db: __MODULE__, persist_db: "./db/data"
 
   @impl GenServer
-  def init(_) do
-    File.mkdir_p!(@persist_db)
-    {:ok, nil}
+  def init(%Database{persist_db: persist_db} = initial_args) do
+    File.mkdir_p!(persist_db)
+    {:ok, initial_args}
   end
 
   @impl GenServer
-  def handle_call({:get, key}, _, state) do
-    todos =
-      file_name(key)
-      |> File.read!()
-      |> :erlang.binary_to_term()
-
-    %Todo.List{} = todos
-
-    {:reply, todos, state}
+  def handle_call({:get, key}, _, %Database{} = state) do
+    with {:ok, contents} <- File.read(file_name(state, key)) do
+      todos = :erlang.binary_to_term(contents)
+      {:reply, todos, state}
+    else
+      _ -> {:reply, nil, state}
+    end
   end
 
   @impl GenServer
-  def handle_cast({:store, key, value}, state) do
-    file_name(key)
-    |> File.write!(:erlang.term_to_binary(value))
+  def handle_cast({:store, key, value}, %Database{} = state) do
+    File.write!(file_name(state, key), :erlang.term_to_binary(value))
 
     {:noreply, state}
   end
 
-  def start() do
-    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  def start(args \\ %Database{}) do
+    GenServer.start(__MODULE__, args, name: args.db)
   end
 
-  def store(key, value) do
-    GenServer.cast(__MODULE__, {:store, key, value})
+  def store(state \\ %Database{}, key, value) do
+    GenServer.cast(state.db, {:store, key, value})
   end
 
-  def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+  def get(state \\ %Database{}, key) do
+    GenServer.call(state.db, {:get, key})
   end
 
-  def file_name(key) do
-    Path.join([@persist_db, key])
+  def file_name(state \\ %Database{}, key) do
+    Path.join([state.persist_db, key])
   end
 end
--- test/todo_database_test.exs
+++ test/todo_database_test.exs
@@ -2,8 +2,16 @@ defmodule TodoDatabaseTest do
   use ExUnit.Case
   alias Todo.Database
 
-  setup do
-    Database.start()
+  setup %{test: test_name} do
+    db = "db.#{to_string(test_name)}" |> String.to_atom()
+    persist_db = "./db/data/test.#{to_string(test_name)}"
+
+    database = %Database{
+      db: db,
+      persist_db: persist_db
+    }
+
+    {:ok, _} = Database.start(database)
 
     alice_todo_list_name = "alice.#{:rand.uniform()}"
 
@@ -14,18 +22,18 @@ defmodule TodoDatabaseTest do
         %{date: ~D[2023-05-02], title: "sing a song"}
       ])
 
-    Database.store(alice_todo_list_name, todos)
+    Database.store(database, alice_todo_list_name, todos)
 
     on_exit(fn ->
-      GenServer.stop(Database, :normal)
-      File.rm_rf!(Database.file_name(alice_todo_list_name))
+      GenServer.stop(database.db, :normal)
+      File.rm_rf!(Database.file_name(database, alice_todo_list_name))
     end)
 
-    %{alice: alice_todo_list_name}
+    %{alice: alice_todo_list_name, database: database}
   end
 
-  test "can read a todo list from the database", %{alice: alice} do
-    assert Database.get(alice) == %Todo.List{
+  test "can read a todo list from the database", %{alice: alice, database: database} do
+    assert Database.get(database, alice) == %Todo.List{
              auto_id: 4,
              entries: %{
                1 => %{date: ~D[2023-05-03], id: 1, title: "get groceries"},
@@ -36,11 +44,11 @@ defmodule TodoDatabaseTest do
            }
   end
 
-  test "can write a todo list to the database", %{alice: alice} do
+  test "can write a todo list to the database", %{alice: alice, database: database} do
     # need this so that we're confident that the .store cast is processed by then
     # otherwise this test fails because the new file hasn't been created yet
-    Database.get(alice)
+    Database.get(database, alice)
 
-    assert File.exists?(Database.file_name(alice))
+    assert File.exists?(Database.file_name(database, alice))
   end
 end
